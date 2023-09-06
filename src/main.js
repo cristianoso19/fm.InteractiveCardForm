8678e4e7f239cab75d34d3d210ceb3447d6b4f66
cristian <cristianoso19@gmail.com>
2023-09-06 18:26:53 -0500

    Implement year and month regex js validation

--- src/main.js
+++ src/main.js
@@ -1,19 +1,49 @@
 const form = document.querySelector("form");
-const month = document.getElementById('month');
-const year = document.getElementById('year');
-const cvc = document.getElementById('cvc');
+
 const error_date = document.getElementById('error-date');
 const error_cvc = document.getElementById('error-cvc');
 const card_name = document.getElementById('card-name');
 const card_number = document.getElementById('card-number');
-const cardholder_name = document.getElementById('cardholder-name');
-const cardholder_number = document.getElementById('cardholder-number');
 const card_month = document.getElementById('card-month');
 const card_year = document.getElementById('card-year');
 const card_cvc = document.getElementById('card-cvc');
+const cardholder_name = document.getElementById('cardholder-name');
+const cardholder_number = document.getElementById('cardholder-number');
+const cardholder_month = document.getElementById('cardholder-month');
+const cardholder_year = document.getElementById('cardholder-year');
+const cardholder_cvc = document.getElementById('cardholder-cvc');
 const cardholder_name_error = document.getElementById('cardholder-name-error');
 const cardholder_number_error = document.getElementById('cardholder-number-error');
 
+cardholder_month.addEventListener('focusout', ()=>{
+  if (cardholder_month.value != "") {
+    if (validateMonth(cardholder_month.value)) {
+      console.log("bien");
+    } else {
+      cardholder_month.classList = null;
+      cardholder_month.classList.add("error-input");
+      error_date.classList = null;
+      error_date.classList.add("error");
+      error_date.textContent = "Can't be blank.";
+      console.log("mal");
+    }
+  } else {
+    cardholder_month.classList = null;
+    cardholder_month.classList.add("error-input");
+    error_date.classList = null;
+    error_date.classList.add("error");
+    error_date.textContent = "Can't be blank.";
+  }
+})
+
+function validateMonth (value){
+  let regexPattern =/0[1-9]|1[0-2]/;
+  return regexPattern.test(value) ? true : false;  
+}
+function validateYear (value){
+  let regexPattern =/[0-9]{2}/;
+  return regexPattern.test(value) ? true : false;  
+}
 
 cardholder_name.addEventListener('input',()=>{
   cardholder_name.value = cardholder_name.value.toUpperCase();
@@ -98,68 +128,68 @@ function ValidateCreditCardNumber() {
   }
 }
 
-cvc.addEventListener('input',(event)=>{
-  if (cvc.validity.valid){
-    quitErrors(cvc, error_cvc);
-    card_cvc.textContent = cvc.value;
-  } else {
-    showError(cvc,error_cvc);
-  }
-});
+// cvc.addEventListener('input',(event)=>{
+//   if (cvc.validity.valid){
+//     quitErrors(cvc, error_cvc);
+//     card_cvc.textContent = cvc.value;
+//   } else {
+//     showError(cvc,error_cvc);
+//   }
+// });
 
-year.addEventListener('input',(event)=>{
-  if (year.validity.valid){
-    quitErrors(year,error_date);
-    card_year.textContent = year.value;
-  } else {
-    showError(year,error_date);
-  }
-});
+// year.addEventListener('input',(event)=>{
+//   if (year.validity.valid){
+//     quitErrors(year,error_date);
+//     card_year.textContent = year.value;
+//   } else {
+//     showError(year,error_date);
+//   }
+// });
 
-month.addEventListener("input", (event) => {
-  // Each time the user types something, we check if the
-  // form fields are valid.
-  if (month.validity.valid) {
-    // In case there is an error message visible, if the field
-    // is valid, we remove the error message.
-    quitErrors(month,error_date)
-    card_month.textContent = month.value;
-  } else {
-    // If there is still an error, show the correct error
-    showError(month,error_date);
-  }
-});
+// month.addEventListener("input", (event) => {
+//   // Each time the user types something, we check if the
+//   // form fields are valid.
+//   if (month.validity.valid) {
+//     // In case there is an error message visible, if the field
+//     // is valid, we remove the error message.
+//     quitErrors(month,error_date)
+//     card_month.textContent = month.value;
+//   } else {
+//     // If there is still an error, show the correct error
+//     showError(month,error_date);
+//   }
+// });
 
-form.addEventListener("submit", (event) => {
-  // if the email field is valid, we let the form submit
-  if (!month.validity.valid) {
-    // If it isn't, we display an appropriate error message
-    showError(month,error_date);
-    // Then we prevent the form from being sent by canceling the event
-    event.preventDefault();
-  }
-});
+// form.addEventListener("submit", (event) => {
+//   // if the email field is valid, we let the form submit
+//   if (!month.validity.valid) {
+//     // If it isn't, we display an appropriate error message
+//     showError(month,error_date);
+//     // Then we prevent the form from being sent by canceling the event
+//     event.preventDefault();
+//   }
+// });
 
-function showError(input,error) {
-  if (input.validity.valueMissing){
-    renderErrors(input,error,"Can't be blank.");
-  } else {
-    renderErrors(input,error,"Wrong format, numbers only.");
-  }
-}
+// function showError(input,error) {
+//   if (input.validity.valueMissing){
+//     renderErrors(input,error,"Can't be blank.");
+//   } else {
+//     renderErrors(input,error,"Wrong format, numbers only.");
+//   }
+// }
 
-function renderErrors(input,error,message ){
-  // Set the styling appropriately
-  error.textContent = message; // Reset the content of the message
-  error.className = "error";
-  input.className = "error-input";
-}
+// function renderErrors(input,error,message ){
+//   // Set the styling appropriately
+//   error.textContent = message; // Reset the content of the message
+//   error.className = "error";
+//   input.className = "error-input";
+// }
 
-function quitErrors(input, error) {
-  // Reset the content of the message
-  error.textContent = "";
-  // Reset the visual state of the message
-  error.className = "hidden";
-  input.className = "input-date";
-}
+// function quitErrors(input, error) {
+//   // Reset the content of the message
+//   error.textContent = "";
+//   // Reset the visual state of the message
+//   error.className = "hidden";
+//   input.className = "input-date";
+// }
 
